93c6aee8505c4ed59fa4e3b06afe417d6c8c8499
Kavit2102 <kavitdesai212002@gmail.com>
2024-03-10 10:06:24 +0530

    server

---
+++ server/app.js
@@ -0,0 +1,34 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import cors from "cors";
+import connectDB from "./config/connectdb.js";
+import userRoutes from "./routes/userRoutes.js";
+
+const app = express();
+const port = process.env.PORT;
+const DATABASE_URL = process.env.DATABASE_URL;
+
+//cors policy
+app.use(cors({
+  "origin": "*",
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "preflightContinue": false,
+  "optionsSuccessStatus": 204
+}));
+app.use(express.json());
+
+//load routes
+app.use("/api/gymbook", userRoutes);
+
+// /**
+//  * Connects to the database using the provided URL and starts the server listening on the specified port.
+//  * @returns None
+//  */
+async function main() {
+  await connectDB(DATABASE_URL);
+  app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+  });
+}
+main();
