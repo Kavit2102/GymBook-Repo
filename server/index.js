83ded574a55cccdf352cdf8de2703be083d948a6
Kavit2102 <kavitdesai212002@gmail.com>
2024-03-10 11:41:55 +0530

    src

--- server/index.js
+++ server/index.js
@@ -2,8 +2,8 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import cors from "cors";
-import connectDB from "./config/connectdb.js";
-import userRoutes from "./routes/userRoutes.js";
+import connectDB from "./src/config/connectdb.js";
+import userRoutes from "./src/routes/userRoutes.js";
 
 const app = express();
 const port = process.env.PORT;
@@ -20,8 +20,9 @@ app.use(express.json());
 
 //load routes
 app.use('/', (req, res) => {
-  res.render("backend")
+  res.json({ "messaage": "backend" });
 })
+
 app.use("/api/gymbook", userRoutes);
 
 // /**
